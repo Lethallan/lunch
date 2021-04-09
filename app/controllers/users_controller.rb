d56de850a20090b1918352cc4edfa0725d2cedf1
Lethallan <dk17march@gmail.com>
2021-04-09 17:04:57 +0300

    users can edit their profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: %i[show]
+  before_action :find_user, only: %i[show edit update]
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
